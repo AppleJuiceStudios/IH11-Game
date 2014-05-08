d6cfb506c53b10f3a338e6b44a215cf8cc1863da
Tobias Bodewig <tbodewig@web.de>
2014-05-08 09:11:11 +0200

    Adding "The Number".

--- IH11-Game/src/Main.java
+++ IH11-Game/src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 
+	public static final int theNumber = 42;
+	
 	public static void main(String[] args) {
 		System.out.println("Test: Philipp");
 		System.out.println("Test: Hi!");
