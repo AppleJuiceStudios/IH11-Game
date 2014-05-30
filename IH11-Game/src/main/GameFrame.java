19f336de0976eb8460e0020fb36de8aea0255404
Philipp Auch <philipp.auch@web.de>
2014-05-30 20:56:51 +0200

    Chaging Start Position

--- IH11-Game/src/main/GameFrame.java
+++ IH11-Game/src/main/GameFrame.java
@@ -2,21 +2,22 @@ package main;
 
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame{
+public class GameFrame extends JFrame {
 
 	private GamePanel gamePanel;
-	
-	public GameFrame(){
+
+	public GameFrame() {
 		init();
 	}
-	
-	public void init(){
+
+	public void init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gamePanel = new GamePanel();
 		setContentPane(gamePanel);
 		setResizable(false);
 		pack();
 		setVisible(true);
+		setLocationRelativeTo(null);
 	}
-	
+
 }
