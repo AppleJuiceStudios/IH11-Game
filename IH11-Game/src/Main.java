e401bfad9bfe4fddc3888eedb334cfb4c81cc859
PhilippvK <phvankempen@googlemail.com>
2014-05-30 15:41:19 +0200

    Main leeren

--- IH11-Game/src/Main.java
+++ IH11-Game/src/Main.java
@@ -1,11 +1,6 @@
 
-public class Main {
-
-	public static final int theNumber = 42;
-	
+public class Main {	
 	public static void main(String[] args) {
-		System.out.println("Test: Philipp");
-		System.out.println("Test: Hi!");
-		System.out.println("Hi"); 
+
 	}
 }
