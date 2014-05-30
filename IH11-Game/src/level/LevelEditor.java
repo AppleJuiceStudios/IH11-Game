e95b007d28e0fb541cfd0dd2471dc93567a73b9d
Philipp Auch <philipp.auch@web.de>
2014-05-30 23:32:10 +0200

    Implementing Audio support

--- IH11-Game/src/level/LevelEditor.java
+++ IH11-Game/src/level/LevelEditor.java
@@ -5,12 +5,12 @@ public class LevelEditor {
 	public static void main(String[] args) {
 		Level level = new Level();
 		byte[][] byteArr = new byte[][] { 
-			new byte[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 4 },
-			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
-			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
-			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
-			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
-			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
+			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 9, 9, 9, 0, 1, 2, 9, 9, 9, 9 },
+			new byte[] { 9, 9, 9, 3, 4, 8, 9, 9, 9, 9 },
+			new byte[] { 9, 9, 9, 6, 7, 9, 9, 9, 9, 9 },
+			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
 			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
 			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
 			new byte[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 },
@@ -31,5 +31,4 @@ public class LevelEditor {
 		level.setTileSet(byteArrNew);
 		level.save("ExampleLevel");
 	}
-
 }
