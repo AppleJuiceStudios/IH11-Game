3d24a9e3a37bfb125fe5ba7bfc697e4c3526d5b0
Philipp Auch <philipp.auch@web.de>
2014-05-31 03:08:29 +0200

    Adding new Level

--- IH11-Game/src/level/LevelEditor.java
+++ IH11-Game/src/level/LevelEditor.java
@@ -5,16 +5,16 @@ public class LevelEditor {
 	public static void main(String[] args) {
 		Level level = new Level();
 		byte[][] byteArr = new byte[][] { 
-			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 0, 1, 2, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 3, 4, 5, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 6, 7, 8, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
-			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 1, 1 },
-			new byte[] { 4, 2, 9, 9, 9, 9, 9, 9, 9, 0, 4, 4, 4 },
-			new byte[] { 4, 4, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4 },
-			new byte[] { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 }
+			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 0, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 6, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 5, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 1, 1, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 4, 2, 9, 9, 9, 9, 9, 9, 9, 0, 4, 4, 4, 9, 9, 9, 9, 9, 9, 9, 9 },
+			new byte[] { 4, 4, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 },
+			new byte[] { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 }
 		};
 		byte[][] byteArrNew = new byte[byteArr[0].length][byteArr.length];
 
@@ -26,6 +26,6 @@ public class LevelEditor {
 		level.setTileSet(byteArrNew);
 		level.setStartPositionX(32.0);
 		level.setStartPositionY(32.0);
-		level.save("res/data/levels/Level1");
+		level.save("res/data/levels/Level3");
 	}
 }
